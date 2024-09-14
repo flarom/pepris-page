d532168108a6d4f7411e7b4c438604451f48239a
flarom <151688146+flarom@users.noreply.github.com>
2024-09-14 20:57:04 +0000

    added 404 page, added codeblocks

--- script.js
+++ script.js
@@ -19,4 +19,28 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+function addCopyButtonToCodeblock() {
+    const codeblocks = document.querySelectorAll('textarea#codeblock');
 
+    codeblocks.forEach((codeblock) => {
+        const copyButton = document.createElement('button');
+        copyButton.textContent = 'Copy code';
+        copyButton.classList.add('copy-btn');
+
+        copyButton.addEventListener('click', () => {
+            codeblock.select();
+            document.execCommand('copy');
+            copyButton.textContent = 'Copied!';
+
+            setTimeout(() => {
+                copyButton.textContent = 'Copy code';
+            }, 2000);
+        });
+
+        const container = codeblock.closest('.code-container');
+        container.appendChild(copyButton);
+    });
+}
+
+// Chama a função quando a página carregar
+window.onload = addCopyButtonToCodeblock;
